f3a918dfc632c30ccb8c074c287c3d5b3b6c013c
Sean McWatters <mcwatterssean@gmail.com>
2021-12-09 11:51:11 -0500

    init

---
+++ ClassesExercise1/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesExercise1
+{
+    internal class Car
+    {
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+        public Car()
+        {
+
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }    
+    }
+}
---
+++ ClassesExercise1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassesExercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCar = new Car()
+            {
+                Make = "Hyundai",
+                Model = "Sonata",
+                Year = 1999
+            };
+
+            Car hisCar = new Car();
+            hisCar.Make = "Ford";
+            hisCar.Model = "Focus";
+            hisCar.Year = 2013;
+
+            var chevy = new Car( "Chevy", "Impala", 2001);
+
+            var carList = new List<Car>() { myCar, hisCar, chevy };
+
+            foreach (var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make} { vehicle.Model} { vehicle.Year}");
+            }    
+
+
+
+        }
+    }
+}
